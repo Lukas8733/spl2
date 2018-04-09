0bd22ab77c981ccd91a364c0f0088597051c67b7
Lukas8733 <171wita05@o365.htl-leoben.at>
2018-04-09 15:24:22 +0200

    Änderung7

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -11,3 +11,11 @@ for p in range(0,int(z)):
         print("Es sind zu viele Leute im Bus!")
         r = k-60
         print("Es müssen", r,"Leute austeigen")
+    if(k<0):
+        print("Es sind zu wenig Leute im Bus!")
+        t = 0-k
+        u = input("Wie viele Leute steigen ein?")
+        q = input("Wie viele Leute steigen aus?")
+        k = k +int(u)
+        k = k -int(q)
+        print("Es sind", k,"Leute im Bus")
